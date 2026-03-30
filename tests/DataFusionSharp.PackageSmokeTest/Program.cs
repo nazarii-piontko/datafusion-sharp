b0262d7593dfe24d3f114cc54beda228795787d0
Nazarii Piontko <1506550+nazarii-piontko@users.noreply.github.com>
2026-03-30 20:08:51 +0200

    feat: add DataFusionSharp.Data NuGet package

--- tests/DataFusionSharp.PackageSmokeTest/Program.cs
+++ tests/DataFusionSharp.PackageSmokeTest/Program.cs
@@ -1,10 +1,24 @@
-﻿using DataFusionSharp;
+﻿using Dapper;
+using DataFusionSharp;
+using DataFusionSharp.Data;
 
 using var runtime = DataFusionRuntime.Create();
 using var context = runtime.CreateSessionContext();
+
+// Test DataFusionSharp API
+Console.WriteLine("=== Test DataFusionSharp API ===");
 
 using var df = await context.SqlAsync("SELECT s.value AS id, sin(s.value) AS value FROM generate_series(1, 10) AS s");
 
 await df.ShowAsync();
 
 Console.WriteLine($"Total rows: {await df.CountAsync()}");
+
+// Test DataFusionSharp.Data API
+Console.WriteLine("=== Test DataFusionSharp.Data API ===");
+
+await using var connection = context.AsConnection(leaveOpen: true);
+
+var ids = await connection.QueryAsync<long>("SELECT s.value AS id FROM generate_series(1, 10) AS s");
+
+Console.WriteLine($"IDs: {string.Join(", ", ids)}");
